063554e89a3c77bd5fc59aa138088b0d79860011
unknown <donein1989@gmail.com>
2024-05-31 05:18:01 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,94 +1,56 @@
 import socket
-from threading import Thread
-REDIS_PONG = "+PONG\r\n"
-DEBUG = False
-DELIM = "\r\n"
-REDIS_PONG = "+PONG" + DELIM
-REDIS_COMMANDS = [
-    # REDIS_PONG
-    "COMMAND",
-    "PING",
-    "ECHO",
-]  # Asterisk as first byte; int for num elements in array; CRLF; each RESP
-# def handle_data(data):
-#     resp = "-ERR unknown command\r\n"
-#     #    print(f"Data received {data}")
-#     data_list = data.splitlines()
-#     if data_list[2].decode("utf8").upper() == "PING":
-#         #        print("PING received")
-#         resp = REDIS_PONG
-#     elif data_list[2].decode("utf8").upper() == "COMMAND":
-#         resp = "*" + str(len(REDIS_COMMANDS)) + "\r\n"
-#         for cmd in REDIS_COMMANDS:
-#             resp += cmd
-def handle_command(data):
-    # set unknown command error
-    resp = "-ERR unknown command" + DELIM
-    data_list = data.decode("utf8").splitlines()
-    num_of_items = int(data_list[0].strip("*"))  # this begins with an *
-    # * <int> - number of items in array
-    # $ <int> - length of command
-    if DEBUG:
-        print(f"Received data {data}")
-    if (
-        num_of_items == (len(data_list) - 1) / 2
-    ):  # remove number of elements, then for each item, the length is supplied
-        # Advised number of items in array matches how many we parsed
-        cmd = data_list[2].upper()
-        cmd_length = int(data_list[1].strip("$"))
-        if not len(cmd) == cmd_length:
-            # we didn't count right
-            print(
-                f"ERROR: advised cmd length does not match what I calculated for {cmd} != {cmd_length}"
-            )
-            return
-        match cmd:
-            case "COMMAND":
-                resp = "*" + str(len(REDIS_COMMANDS)) + DELIM
-                for cmd in REDIS_COMMANDS:
-                    resp += "+" + cmd + DELIM
-            case "PING":
-                resp = REDIS_PONG
-            case "ECHO":
-                param = data_list[4]
-                param_len = int(data_list[3].strip("$"))
-                if param_len == len(param):
-                    resp = "$" + str(param_len) + DELIM + param + DELIM
-            case _:
-                pass
-    else:
-        print(f"Incorrect number of elements {num_of_items}")
-        resp = f"-ERR number of elements does not match: {num_of_items} <> {len(data_list) - 1}"
-    return resp
-def handle_client(client_socket, addr):
-    print(f"Received new connection {addr}")
-    if DEBUG:
-        print(f"Received new connection {addr}")
-    while client_socket:
-        # print(f"Waiting for data from {addr}")
-        data = client_socket.recv(1024)
-        if len(data) > 0:
-            # resp = handle_data(data)
-            resp = handle_command(data)
+import os
+from threading import *
+class Database:
+    def __init__(self):
+        self.data = {}
+    def add(self, key, value):
+        self.data[key] = value
+    def get(self, key):
+        if key not in self.data:
+            return -1
+        return self.data[key]
+class Connection(Thread):
+    def __init__(self, socket, address):
+        super().__init__()
+        self.socket = socket
+        self.address = address
+        self.database = Database()
+        self.start()
+    def run(self):
+        while True:
+            request = self.socket.recv(1024)
+            if not request:
+                break
+            parsedReq = self.parseReq(request)
+            self.parseCommandAndSendRequest(parsedReq)
+        self.close()
+    def parseReq(self, request):
+        requestParams = request.decode().split("\r\n")
+        return requestParams
+    def parseCommandAndSendRequest(self, request):
+        requestCommand = request[2].lower()
+        if "ping" == requestCommand:
+            dataToSend = "+PONG\r\n"
+        elif "echo" == requestCommand:
+            dataToSend = f"+{request[-2]}\r\n"
+        elif "set" == requestCommand:
+            self.database.add(request[4], request[6])
+            dataToSend = "+OK\r\n"
+        elif "get" == requestCommand:
+            value = self.database.get(request[-2])
+            dataToSend = f"+{value}\r\n"
         else:
-            # print(f"Closing connection {addr}")
             return
-        if resp:
-            # print(f"Sending response to {addr}")
-            # print(resp.encode())
-            if DEBUG:
-                print(f"Sending response {resp.encode()} to {addr}")
-            client_socket.sendall(resp.encode())
-            resp = None
+        self.socket.send(dataToSend.encode())
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.listen()
+    serverSocket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
-        conn, addr = server_socket.accept()  # wait for client
-        t = Thread(target=handle_client, args=(conn, addr))
-        t.start()
-    server_socket.close()
+        # Wait for client
+        clientSocket, clientAddress = serverSocket.accept()
+        print("Received a connection from client: {clientAddress}")
+        Connection(clientSocket, clientAddress)
 if __name__ == "__main__":
     main()
